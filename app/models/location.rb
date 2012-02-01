bc6c71b41f7530953c0a870cbf28e754e266c43e
carl sednaoui <carlsednaoui@gmail.com>
2012-02-01 07:40:30 -0500

    changed the phone numbers

--- app/models/location.rb
+++ app/models/location.rb
@@ -8,7 +8,7 @@ class Location < ActiveRecord::Base
 
     reverse_geocoded_by :latitude, :longitude do |obj,results|
         if geo = results.first
-            obj.city    = geo.city
+            obj.city = geo.city
         end
     end
     after_validation :reverse_geocode
